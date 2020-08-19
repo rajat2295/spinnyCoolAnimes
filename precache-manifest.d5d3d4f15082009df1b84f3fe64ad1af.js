f5739e9cb57e748ff3402c2e5521e2690655bc07
rajat <rajat.ratra@99acres.com>
2020-08-20 02:36:22 +0530

    Updates

---
+++ precache-manifest.d5d3d4f15082009df1b84f3fe64ad1af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc347a93e975ecc126eb6258d718a2f",
+    "url": "/spinnyCoolAnimes/index.html"
+  },
+  {
+    "revision": "17f7d658d50d4451623c",
+    "url": "/spinnyCoolAnimes/static/css/main.7b468b7b.chunk.css"
+  },
+  {
+    "revision": "4cd03ab4f55d3d65f54f",
+    "url": "/spinnyCoolAnimes/static/js/2.7cf6869d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spinnyCoolAnimes/static/js/2.7cf6869d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f7d658d50d4451623c",
+    "url": "/spinnyCoolAnimes/static/js/main.bc030ec3.chunk.js"
+  },
+  {
+    "revision": "ca5409ef23e0d02eca61",
+    "url": "/spinnyCoolAnimes/static/js/runtime-main.e785e466.js"
+  }
+]);
