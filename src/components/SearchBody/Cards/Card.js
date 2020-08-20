743f658b515a7b2240efe88a2576cd3f00934729
rajat <rajat.ratra@99acres.com>
2020-08-20 23:08:34 +0530

    FIX | backup image added

--- src/components/SearchBody/Cards/Card.js
+++ src/components/SearchBody/Cards/Card.js
@@ -7,7 +7,7 @@ const Card = ({url='',imageUrl='',name=''}) => {
     return (
             <div href={url} className="card">
                         <div className="card__image">
-                            <img loading="lazy" src={imageUrl} alt={name} />
+                            <img style={{backgroundImage:`url(${process.env.PUBLIC_URL}/images/tenor.gif)`}} loading="lazy" src={imageUrl} alt={name} />
                         </div>
                         <div className="card__name">
                             <div><span>{name}</span></div> 
